33dc7ab67c042535b34c85d904505a0d059f28ad
AndresMardonesUACH <andres.mardones@alumnos.uach.cl>
2026-05-24 17:25:39 -0400

    tests optimizados en tiempo

--- tests/test_caso.py
+++ tests/test_caso.py
@@ -24,32 +24,25 @@ def crear_proyecto(driver, nombre, archivo):
     """
      # Ver en qué URL estamos
     print(f"URL inicial: {driver.current_url}")
-    time.sleep(2)
     
     #Presionar "Crear Proyecto"
     driver.find_element(By.ID, "crear_proyecto").click()
-    time.sleep(1)
     print(f"URL inicial: {driver.current_url}")
 
     #Ingresar nombre del proyecto
     driver.find_element(By.NAME, "nombre_proyecto").send_keys(nombre)
-    time.sleep(1)
 
     #Subir el archivo excel "plantilla.xlsx" descargado en el paso 3
     driver.find_element(By.NAME, "archivo").send_keys(archivo)
-    time.sleep(1)
 
     #Presionar "+ Crear Proyecto"
     driver.find_element(By.ID, "crear_proyecto").click()
-    time.sleep(2)
 
     #Presionar "Confirmar"
     driver.find_element(By.ID, "btn-confirmar").click()
-    time.sleep(2)
 
     #Ingresar al proyecto recién creado
     driver.find_element(By.ID,nombre).click()
-    time.sleep(2)
 
 
 
@@ -64,32 +57,29 @@ def modificar_proyecto(driver, nombre):
     """
     # Ver en qué URL estamos
     print(f"URL inicial: {driver.current_url}")
-    time.sleep(2)
 
     #Ingresar al proyecto creado en el caso 1
     driver.find_element(By.ID, nombre).click()
-    time.sleep(1)
 
     #Seleccionar "Lista" de la navbar
     driver.find_element(By.ID, "nav-lista").click()
-    time.sleep(1)   
 
     #Presionar el icono de lápiz al lado derecho de una actividad
     driver.find_element(By.CLASS_NAME, "btn-editar").click()
-    time.sleep(1)
+    
 
     #Modificar el texto en el input nombre situado primero
     driver.find_element(By.ID, "editNombreInput").clear()
     driver.find_element(By.ID, "editNombreInput").send_keys("Actividad modificada")
-    time.sleep(1)   
+    
 
     #Presionar "Guardar"
     driver.find_element(By.ID, "saveChanges").click()
-    time.sleep(1)
+    
     wait = WebDriverWait(driver, 10)
     alert = wait.until(EC.alert_is_present())
     alert.accept()
-    time.sleep(2)
+    
 
 
 @pytest.mark.parametrize("setup_crear_proyecto", [("Proyecto A"), ("Proyecto C")], indirect=True)
